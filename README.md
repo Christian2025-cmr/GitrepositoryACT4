77d1397fbf4940698ec8757c30cf9e92c05559db
Christian2025-cmr <christianregacho20@gmail.com>
2025-08-16 06:59:32 +0000

    calculator

--- README.md
+++
@@ -1,1 +0,0 @@
-# GitrepositoryACT4
---
+++ calculator.dart
@@ -0,0 +1,17 @@
+void main() {
+  // Define two numbers
+  double num1 = 10;
+  double num2 = 5;
+
+  // Perform operations
+  double addition = num1 + num2;
+  double subtraction = num1 - num2;
+  double multiplication = num1 * num2;
+  double division = num1 / num2;
+
+  // Print results
+  print("Addition: $num1 + $num2 = $addition");
+  print("Subtraction: $num1 - $num2 = $subtraction");
+  print("Multiplication: $num1 * $num2 = $multiplication");
+  print("Division: $num1 / $num2 = $division");
+}
